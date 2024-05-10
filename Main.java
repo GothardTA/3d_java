c5d31d6c52ae4271fdb0176437b45727d2cffda7
GothardTA <gothardta@s.dcsdk12.org>
2024-05-10 12:13:37 -0600

    Multiple objects can now be rendered, they are displayed from back to front mostly correctly

--- Main.java
+++ Main.java
@@ -20,7 +20,7 @@ public class Main extends JFrame {
 	private static double fov = 60.0;
 	private static double scale = 20.0;
 	private static double mouseSensitivity = 0.7;
-	private static Object3D stair = new Object3D("./res/objects/cutcube.obj");
+	private static ArrayList<Object3D> objects = new ArrayList<Object3D>();
 
 	Random rand = new Random();
 
@@ -33,152 +33,31 @@ public class Main extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 		super.addMouseMotionListener( new MyMouseListener() );
+	
+		Object3D object = new Object3D("./res/objects/cutcube.obj", new Vector3d(0, 0, 0), Color.GREEN);
+		object.scaleAll(scale);
+		objects.add(object);
 
-		vertexes = ParseOBJ.getVertexesFromFile("./res/objects/cutcube.obj");
-		triangles = ParseOBJ.getTrianglesFromFile("./res/objects/cutcube.obj");
+		object = new Object3D("./res/objects/cube.obj", new Vector3d(-1, 0, 0), Color.RED);
+		object.scaleAll(scale);
+		objects.add(object);
 
-		for (Vector3d vertex : vertexes) {
-			vertex.scale(scale);
-		}
-    }
- 
-    void drawLines(Graphics g) {
-        // Graphics2D g2d = (Graphics2D) g;
-		sortTriangles(triangles);
-		// BufferedImage img = ImageIO.read( new File(".\\res\\img\\card.png") );
- 
-        for (int[] triangle : triangles) {
-			Graphics2D g2 = (Graphics2D) g;
-
-			Vector3d vertex1 = vertexes.get(triangle[0]-1);
-			Vector3d vertex2 = vertexes.get(triangle[1]-1);
-			Vector3d vertex3 = vertexes.get(triangle[2]-1);
-
-			double[] first = vertex1.perspective2D(cameraPos, cameraAngle, WIDTH, HEIGHT, fov);
-			double[] second = vertex2.perspective2D(cameraPos, cameraAngle, WIDTH, HEIGHT, fov);
-			double[] third = vertex3.perspective2D(cameraPos, cameraAngle, WIDTH, HEIGHT, fov);
-
-			g2.setColor(new Color(rand.nextInt(3)<<6, rand.nextInt(3)<<6, rand.nextInt(3)<<6));
-
-			Path2D triShape = new Path2D.Double();
-			triShape.moveTo((int) first[0], (int) first[1]);
-			triShape.lineTo((int) second[0], (int) second[1]);
-			triShape.lineTo((int) third[0], (int) third[1]);
-			triShape.closePath();
-			g2.fill(triShape);
-
-			// BufferedImage strImg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_ARGB);
-			// Graphics2D g2d = strImg.createGraphics();
-			// g2d.setClip(triShape);
-			// g2d.drawImage(img, 0, 0, null);
-			// g2d.dispose();
-
-			// g.setColor(Color.BLACK);
-			// g.fillPolygon(
-			// 	new int[] {(int) first[0], (int) second[0], (int) third[0]},
-			// 	new int[] {(int) first[1], (int) second[1], (int) third[1]},
-			// 	3
-			// );
-
-
-			// g.setColor(new Color(255, 0, 0));
-			// g.drawLine((int) first[0], (int) first[1], (int) second[0], (int) second[1]);
-			// g.drawLine((int) second[0], (int) second[1], (int) third[0], (int) third[1]);
-			// g.drawLine((int) third[0], (int) third[1], (int) first[0], (int) first[1]);
-
-		}
-		
-		
+		object = new Object3D("./res/objects/cutcube.obj", new Vector3d(-1, -1, 0), Color.BLUE);
+		object.scaleAll(scale);
+		objects.add(object);
     }
  
     public void paint(Graphics g) {
         super.paint(g);
-        drawLines(g);
+
+		Object3D.sortObjects(objects, cameraPos);
+        for (Object3D object : objects) {
+			object.drawToScreen(g, cameraPos, cameraAngle, WIDTH, HEIGHT, fov);
+		}
+
+		g.setColor(Color.BLACK);
 		g.drawString("(" + cameraPos.getX() + ", " + cameraPos.getY() + ", " + cameraPos.getZ() + ")", 100, 100);
     }
-
-	// sorts them from back to front
-	private static void sortTriangles(ArrayList<int[]> tris) {
-		// MUST DO:
-		// Sort triangles from farthest to camera to cloest to camera
-		// currently sorts by z value
-		ArrayList<int[]> newTris = new ArrayList<int[]>();
-		// for (int i = 0; i < tris.size(); i++) {
-		// 	int[] tri = tris.get(i);
-		// 	double averageZ = (vertexes.get(tri[0]-1).getZ() + vertexes.get(tri[1]-1).getZ() + vertexes.get(tri[2]-1).getZ()) / 3.0;
-
-		// 	if (newTris.size() == 0) {
-		// 		newTris.add(tri);
-		// 	} else {
-		// 		for (int j = 0; j < newTris.size(); j++) {
-		// 			int[] tmpTri = newTris.get(j);
-		// 			double tmpAverageZ = (vertexes.get(tmpTri[0]-1).getZ() + vertexes.get(tmpTri[1]-1).getZ() + vertexes.get(tmpTri[2]-1).getZ()) / 3.0;
-		// 			if (averageZ > tmpAverageZ) {
-		// 				newTris.add(j, tri);
-		// 				break;
-		// 			}
-		// 		}
-		// 	}
-		// }
-
-		for (int[] tri : tris) {
-			double distance1 = vertexes.get(tri[0]-1).getDistanceFromPoint(cameraPos);
-			double distance2 = vertexes.get(tri[1]-1).getDistanceFromPoint(cameraPos);
-			double distance3 = vertexes.get(tri[2]-1).getDistanceFromPoint(cameraPos);
-			double average = (distance1 + distance2 + distance3) / 3;
-
-			if (newTris.size() == 0) {
-				newTris.add(tri);
-				continue;
-			}
-
-			boolean added = false;
-
-			for (int i = 0; i < newTris.size(); i++) {
-				double otherDistance1 = vertexes.get(newTris.get(i)[0]-1).getDistanceFromPoint(cameraPos);
-				double otherDistance2 = vertexes.get(newTris.get(i)[1]-1).getDistanceFromPoint(cameraPos);
-				double otherDistance3 = vertexes.get(newTris.get(i)[2]-1).getDistanceFromPoint(cameraPos);
-				double otherAverage = (distance1 + distance2 + distance3) / 3;
-
-				if (average < otherAverage) {
-					newTris.add(i, tri);
-					added = true;
-					break;
-				}
-			}
-
-			if (!added) {
-				newTris.add(tri);
-			}
-		}
-
-		for (int i = 0; i < tris.size(); i++) {
-			tris.set(i, newTris.get(tris.size() - i - 1));
-		}
-	}
-
-	// private static void rotateAllPoints(double x, double y, double z) {
-	// 	for (Vector3d vertex : vertexes) {
-	// 		vertex.setRX(x);
-	// 		vertex.setRY(y);
-	// 		vertex.setRZ(z);
-	// 	}
-	// }
-
-	// private static void rotateAllPointsAroundPoint(Vector3d point, double x, double y, double z) {
-	// 	for (Vector3d vertex : vertexes) {
-	// 		vertex.rotateXAroundPoint(point, x);
-	// 		vertex.rotateYAroundPoint(point, y);
-	// 		vertex.rotateZAroundPoint(point, z);
-	// 	}
-	// }
-
-	// private static void resetRotateAllPoints() {
-	// 	for (Vector3d vertex : vertexes) {
-	// 		vertex.reset();
-	// 		vertex.scale(scale);
-	// 	}
-	// }
 
 	class CustomKeyListener implements KeyListener {
 		public void keyTyped(KeyEvent e) {
